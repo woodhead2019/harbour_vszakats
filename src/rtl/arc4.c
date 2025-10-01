8bfdf3087fe9f0bbc43dc950339f8c9dcc99719f
Viktor Szakats <commit@vsz.me>
2025-10-01 18:34:05 +0200

    arc4: fix for newer glibc on Linux
    Credits-to:
    
    2020-11-03 10:37 UTC+0100 Aleksander Czajczynski (hb fki.pl)
      * src/rtl/arc4.c
        * updated to the current state of glibc, versions >= 2.30 have
          no sysctl(). Should fix build issues on current Linux distros
          like Fedora 33, etc.
    
    2019-01-15 12:50 UTC+0100 Aleksander Czajczynski (hb fki.pl)
      * src/rtl/arc4.c
        ! fix builds on Alpine Linux and possibly others using musl libc,
          which in turn doesn't expose deprecated sysctl() anymore.
    
    Closes #343

--- src/rtl/arc4.c
+++ src/rtl/arc4.c
@@ -54,7 +54,15 @@
       ! defined( HB_OS_ANDROID ) && \
       ! defined( __WATCOMC__ ) && \
       ! defined( __EMSCRIPTEN__ ) )
-#  define HAVE_SYS_SYSCTL_H
+   /*
+    * sysctl() on Linux has fallen into depreciation. Not available in current
+    * runtime C libraries, like musl and glibc >= 2.30.
+    */
+#  if ( ! defined( HB_OS_LINUX ) || \
+      ( ( defined( __GLIBC__ ) && ! ( ( __GLIBC__ > 2 ) || ( ( __GLIBC__ == 2 ) && ( __GLIBC_MINOR__ >= 30 ) ) ) ) ) || \
+      defined( __UCLIBC__ ) )
+#     define HAVE_SYS_SYSCTL_H
+#  endif
 #  define HAVE_DECL_CTL_KERN
 #  define HAVE_DECL_KERN_RANDOM
 #  if defined( HB_OS_LINUX )
